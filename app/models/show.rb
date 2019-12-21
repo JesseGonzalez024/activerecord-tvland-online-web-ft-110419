c210a4c20e9227ba653b8326da2389e4426d9e63
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-12-21 01:54:25 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
   
+  def build_network(args)
+    network = Network.find_by(args)
+    self.network = network || self.network = Network.create(args)
+  end
 end
